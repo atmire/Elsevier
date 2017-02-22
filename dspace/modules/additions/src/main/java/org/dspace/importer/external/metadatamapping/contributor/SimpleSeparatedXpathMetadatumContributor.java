4b66cbfa6117c4cb89e026c7c2c64225e0ef1b6f
Philip Vissenaekens <philipvissenaekens@atmire.com>
2017-02-22 12:46:19 +0100

    39562: support separating api value into multiple DSpace metadata values

---
+++ dspace/modules/additions/src/main/java/org/dspace/importer/external/metadatamapping/contributor/SimpleSeparatedXpathMetadatumContributor.java
@@ -0,0 +1,33 @@
+package org.dspace.importer.external.metadatamapping.contributor;
+
+import java.util.*;
+import java.util.regex.*;
+import org.apache.axiom.om.*;
+import org.dspace.importer.external.metadatamapping.*;
+import org.springframework.beans.factory.annotation.*;
+
+/**
+ * Created by jonas - jonas@atmire.com on 20/05/16.
+ */
+public class SimpleSeparatedXpathMetadatumContributor extends SimpleXpathMetadatumContributor implements MetadataContributor<OMElement> {
+
+    private String separator;
+
+    public String getSeparator() {
+        return separator;
+    }
+
+    @Required
+    public void setSeparator(String separator) {
+        this.separator = Pattern.quote(separator);
+    }
+
+
+    @Override
+    protected void addRetrievedValueToMetadata(List<MetadatumDTO> values, String value) {
+        String[] separatedValues= value.split(separator);
+        for(String valuePart: separatedValues){
+            values.add(getMetadataFieldMapping().toDCValue(getField(), valuePart));
+        }
+    }
+}
